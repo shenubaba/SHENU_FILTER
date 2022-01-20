693ce5d4e7a7356f0614788083584d2e60cacc00
Pasiya Bro <97598362+MASTER-TG@users.noreply.github.com>
2022-01-20 07:35:11 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) @AlbertEinsteinTG
+# (c) @Wuzzup_GirL
 
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+<i>Am Just A Advance Auto Filter Bot..😉🔥
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+Just Add Me To Your Group And Channel And Connect Them And See My Pevers😚
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@Wuzzup_GirL
 </i>"""    
     
     HELP_TEXT = """
@@ -48,11 +48,11 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Wuzzup_GirL">Wuzzup_GirL</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/MASTER-TG/PASIYA-AUTO-FILTER-BOT.git">Click Me</a></i>
 """
